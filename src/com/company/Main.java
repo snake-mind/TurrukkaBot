ad8872c4c7f622d0e52873b95c1a7d84e2c9f6e4
snake-mind <snake-mind@hotmail.com>
2018-07-11 00:29:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+<dependency>
+<groupId>org.telegram</groupId>
+<artifactId>telegrambots</artifactId>
+<version>Latest</version>
+</dependency>
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
